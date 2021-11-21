8e61e54c1683adec8941cc1543de3004e4a743b7
FeyXieXzf <2038855186@qq.com>
2021-11-21 14:14:07 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinUpdateTool
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Updater(Math.Pow(2, 0), args));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Windows Update Tool")]
+[assembly: AssemblyDescription("Windows Update Tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Inc.")]
+[assembly: AssemblyProduct("Windows Update Tool")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Microsoft")]
+[assembly: AssemblyTrademark("Microsoft Inc.")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("adc49d37-3a24-449f-af89-60e48f4d8f97")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
